378ca0ce54f3e67b52128188ddb3d98c6fc0599c
Ismael García <ismael.garcia.torres@gmail.com>
2018-10-07 03:47:56 +0200

    Traer listas dentro de listas en la base de datos. Hay que hacer TODOS !!

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,8 +6,22 @@ const singleton = require('../conexionSingleton');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     const request = new sql.Request(singleton);
-    request.query('SELECT * FROM dbo.Usuario', (err, result) => {
-        res.send(JSON.stringify(result.recordsets[0]));
+    const consulta = `
+    SELECT (
+    
+    -----------
+    SELECT 
+    U.Nombre,
+    U.ClaveEncriptada,
+    U.Email,
+    (SELECT T.Token FROM dbo.TOKEN T WHERE T.Usuario_Nombre = U.Nombre FOR JSON PATH) AS Tokens
+    FROM dbo.Usuario U 
+    -----------
+    
+    FOR JSON PATH) AS Json
+    `; //TODO: Hacer que la conversion de la consulta la haga un helper, y el JSON.parse raro tambien
+    request.query(consulta, (err, result) => {
+        res.send(JSON.parse(result.recordsets[0][0].Json));
     });
 });
 
