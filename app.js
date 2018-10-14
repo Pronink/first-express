8fa3647be9def109819c11ec7e768aa0db82544b
Ismael García <ismael.garcia.torres@gmail.com>
2018-10-14 16:42:32 +0200

    Permitir cross-origin

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Permitir cross-origin
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Rutas
 app.use('/', require('./routes/index'));
 app.use('/usuarios', require('./routes/usuarios'));
